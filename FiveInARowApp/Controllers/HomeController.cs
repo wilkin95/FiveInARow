c7053ab64134b5521e18f13b20bfbdcc83f0add6
DESKTOP-IMV1HK6\twilk <twilkinson@centurytel.net>
2018-02-20 17:56:33 -0600

    Initial push of project

---
+++ FiveInARowApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FiveInARowApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Learning through Literature";
+
+            return View();
+        }
+
+        public ActionResult BuyNow()
+        {
+            ViewBag.Message = "To purchase the curriculum:";
+
+            return View();
+        }
+    }
+}
---
+++ FiveInARowApp/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FiveInARowApp.Models
+{
+    public class Book
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Illustrator { get; set; }
+        public int Copyright { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Topics { get; set; }
+
+
+
+    }
+}
